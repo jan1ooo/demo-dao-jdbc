099af1316189509a5917f2951c2d4239b4864093
jan1ooo <filhojanio67@gmail.com>
2023-04-25 22:04:13 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		Department department = new Department(1, "Books");		
-		System.out.println(department);
+		Department department = new Department(1, "Books");	
+		Seller seller = new Seller(1, "Jonny", "janio@gmail.com", new Date(), 1200.00, department);
+		System.out.println(seller);
 	}
 
 }
